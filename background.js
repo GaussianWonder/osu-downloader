2acb5b60a035972476252087ba03b771c43c33b6
GaussianWonder <teo.virghi@yahoo.ro>
2021-01-25 22:39:03 +0200

    faster download since i changed the way i want manual download to work

--- background.js
+++ background.js
@@ -1,30 +1,13 @@
 // Suppress the default osu download, and replace it
 chrome.downloads.onDeterminingFilename.addListener(function(downloadItem, __suggest) {
+    const regexFilter = /https:\/\/(osu|bm\d*).ppy.sh\//gi;
+
     function suggest(filename, conflictAction) {
         __suggest({filename: filename,
                    conflictAction: conflictAction,
                    conflict_action: conflictAction});
     }
 
-    if(downloadItem.url.includes("https://osu.ppy.sh/beatmapsets/")){ // this means that the download request was done by osu, so we want to overwrite it
-        chrome.downloads.download(
-            {
-                url: downloadItem.finalUrl, // the workaround
-                filename: "osu-songs-symlink/" + downloadItem.filename,
-            }, 
-            function(id) {
-                console.log("new download");
-                // if(downloadIDS.find(_id => _id === id ) != undefined)
-                //     downloadIDS.push(id);
-
-                // manual = false;
-            }
-        );
-
-        chrome.downloads.cancel(downloadItem.id, function(){
-            console.log("cancelled");
-        });
-    }
-    else if (downloadItem.url.includes("ppy.sh/")) // some other osu related download, but not from the main domain
+    if(downloadItem.url.match(regexFilter) !== null)
         suggest("osu-songs-symlink/" + downloadItem.filename);
 });
